adbbedef60cc257c5cab0ba3c03807721fe42263
LGyorgy <l.gyorgy90@gmail.com>
2023-03-04 15:30:45 +0100

    Add Product model and update get action with dummy list

--- InvInsightWebApi/Controllers/ProductsController.cs
+++ InvInsightWebApi/Controllers/ProductsController.cs
@@ -8,9 +8,34 @@ namespace InvInsightWebApi.Controllers
     public class ProductsController : ControllerBase
     {
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
         {
-            return "Hello, world!";
+            var dummyProductList = new List<Product>() {
+                new()
+                {
+                    Id = 1,
+                    Name = "Dummy Product 1",
+                    Sku = "DMMY-PRDCT-1",
+                    Description = "Dummy Product 1 description.",
+                    Category = "Dummies",
+                    Price = 1.99,
+                    Cost = 0.99,
+                    Supplier = "DummySupplier"
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Dummy Product 2",
+                    Description = "Dummy Product 2 description.",
+                    Sku = "DMMY-PRDCT-2",
+                    Category = "Dummies",
+                    Price = 2.99,
+                    Cost = 1.49,
+                    Supplier = "DummySupplier"
+                },
+            };
+
+            return dummyProductList;
         }
     }
 }
---
+++ InvInsightWebApi/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace InvInsightWebApi
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public double Cost { get; set; }
+        public string Supplier { get; set; }
+    }
+}
